1e1e3378894b7367ae5b36fb3418f008a8f698dd
Sumit <sumit.arya@thoughts2binary.com>
2019-09-05 18:28:20 +0530

    inint

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const mongoose = require("mongoose");
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
+const app = express();
+
+// const events = [];
+
+app.use(bodyParser.json());
+
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASS
+    }@cluster0-shard-00-00-znuue.mongodb.net:27017,cluster0-shard-00-01-znuue.mongodb.net:27017,cluster0-shard-00-02-znuue.mongodb.net:27017/${
+      process.env.MONGO_DB
+    }?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(4000, () => console.log(`Node app listening on port 4000!`));
+  })
+  .catch(e => {
+    console.log(e);
+  });
